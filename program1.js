50b53394a5f5ad634d751811a5bb1e325d895ff7
keshariak <aryankeshari2003@gmail.com>
2024-09-30 14:00:28 +0530

    done

--- program1.js
+++ program1.js
@@ -3,9 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = { '(': ')', '{': '}', '[': ']' };
+
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(char); 
+        } else if (stack.length === 0 || map[stack.pop()] !== char) {
+            return false; 
+        }
+    }
+
+    return stack.length === 0; 
 };
 
+// console.log(isValid("{}[])")) false
+// console.log(isValid("{}")) true
+
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -3,8 +3,33 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Map Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    
+    // Loop through the Roman numeral string
+    for (let i = 0; i < s.length; i++) {
+        // If the current value is less than the next one, subtract it
+        if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            total -= romanMap[s[i]];
+        } else {
+            total += romanMap[s[i]];
+        }
+    }
+    
+    return total;
 };
+// console.log(romanToInt("III"));     // Output: 3
+// console.log(romanToInt("LVIII"));   // Output: 58
+// console.log(romanToInt("MCMXCIV")); // Output: 1994
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
